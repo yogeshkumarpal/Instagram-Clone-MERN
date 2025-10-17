b572124c04a8ed59798c3d4fee19444b906d9140
Yogesh Kumar <your-github-yogeshkumarpal43@gmail.com>
2025-10-17 11:33:43 +0530

    Initial commit - MERN Instagram Clone

--- backend/src/config/db/db.js
+++ backend/src/config/db/db.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const mongoURI = process.env.MongoDBURI;
 const connectDB = async () => {
   try {
-    let res = await mongoose.connect("mongodb://0.0.0.0/sasta-insta");
+    let res = await mongoose.connect(mongoURI);
     if (res) {
       console.log("MongoDB connected");
     }
   } catch (error) {
-    console.log("error in co0nnecting DB", error);
+    console.log("error in connecting DB", error);
   }
 };
 
--- backend/src/services/storage.services.js
+++ backend/src/services/storage.services.js
@@ -3,7 +3,7 @@ const imageKit = require("imagekit");
 const storageInstance = new imageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
 const sendFiles = async (file, fileName) => {
