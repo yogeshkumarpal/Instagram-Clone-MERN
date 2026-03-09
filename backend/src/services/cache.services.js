28bf574c6b7ed21e25528e233a7f32b6d11ae39b
yogeshkumarpal <yogeshkumarpal143@gmail.com>
2026-03-09 18:11:20 +0530

    Update cache.services.js

--- backend/src/services/cache.services.js
+++ backend/src/services/cache.services.js
@@ -1,10 +1,9 @@
-const Redis = require("ioredis");
+import Redis from "ioredis";
 
-const cacheClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-});
+const redis = new Redis(process.env.REDIS_URL);
 
-module.exports = cacheClient;
+redis.on("connect", () => console.log("Redis connected"));
+redis.on("error", (err) => console.log("Redis error:", err));
+
+export default redis;
 
